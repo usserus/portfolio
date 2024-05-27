0d245f19a4b928549958e5438fe86dca3e006433
emil.dueringer <s2210456008@fhooe.at>
2024-05-27 15:58:44 +0200

    Init

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  css: ['@/assets/css/roboto.css'],
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: 'jgZ2j5Qjvkc8mNFfXVSPBQtt',
+        apiOptions: {
+          region: '' // Set 'US" if your space is created in US region (EU default)
+        }
+      },
+    ],
+    '@nuxtjs/tailwindcss',
+  ]
+})
