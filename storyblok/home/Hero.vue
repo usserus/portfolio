d762a42ee424541ef809ce54eff4bed20c9b52da
emil.dueringer <s2210456008@fhooe.at>
2024-05-31 12:50:24 +0200

    Hero

---
+++ storyblok/home/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <div class="container">
+    <div v-editable="blok">
+      <h1 class="text-6xl mt-32 font-regular font-mono ml-6">{{ blok.headline }}</h1>
+    </div>
+  </div>
+
+</template>
+
+
+<script setup>
+defineProps({blok: Object})
+</script>
+
+
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: 'Roboto, sans-serif',
+      mono: ['IBM Plex Mono', 'monospace'],
     },
   },
 }
