9d75f0b9cd91bc81a72d5634d91591c0b816d9ed
emil.dueringer <s2210456008@fhooe.at>
2025-09-27 19:04:03 +0200

    feat: add Timeline.vue

---
+++ storyblok/about-me/Text-image.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-editable="blok" class="container mx-auto px-4 py-6 lg:py-14 font-mono font-normal mt-10">
+        <div class="flex flex-col sm:flex-row gap-6">
+            <div class="w-full sm:w-1/2 lg:w-1/4">
+                <img :src="blok.image.filename" :alt="blok.image.alt">
+            </div>
+            <h1 class="xl:w-2/4 text-3xl lg:text-7xl font-mono font-light">
+                {{ blok.heading }}</h1>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({ blok: Object })
+</script>
