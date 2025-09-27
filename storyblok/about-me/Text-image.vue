199734c640f74a9e2ccff805e726257c88b50c70
emil.dueringer <s2210456008@fhooe.at>
2025-09-27 19:56:38 +0200

    feat: adjust layout

--- storyblok/about-me/Text-image.vue
+++ storyblok/about-me/Text-image.vue
@@ -1,11 +1,14 @@
 <template>
-    <div v-editable="blok" class="container mx-auto px-4 py-6 lg:py-14 font-mono font-normal mt-10">
+    <div v-editable="blok" class="container mx-auto px-4 py-6 font-mono font-normal mt-10">
         <div class="flex flex-col sm:flex-row gap-6">
-            <div class="w-full sm:w-1/2 lg:w-1/4">
-                <img :src="blok.image.filename" :alt="blok.image.alt">
+
+            <div class="w-full sm:w-3/4 xl:w-1/4">
+                <img :src="blok.image.filename" :alt="blok.image.alt" class="object-cover">
             </div>
-            <h1 class="xl:w-2/4 text-3xl lg:text-7xl font-mono font-light">
-                {{ blok.heading }}</h1>
+
+            <h1 class="xl:w-2/4 text-3xl md:text-5xl lg:text-7xl font-mono font-light">
+                {{ blok.heading }}
+            </h1>
         </div>
     </div>
 </template>
