48cd9b646834029bad4949c741a5bb7e46bbe3d8
emil.dueringer <s2210456008@fhooe.at>
2025-09-27 19:01:12 +0200

    feat: add daisyUI, format code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 module.exports = {
-  content: ['storyblok/**/*.{vue,js}', 'components/**/*.{vue,js}', 'pages/**/*.vue'],
+  content: [
+    "storyblok/**/*.{vue,js}",
+    "components/**/*.{vue,js}",
+    "pages/**/*.vue",
+  ],
   theme: {
     fontFamily: {
-      sans: 'Roboto, sans-serif',
-      mono: ['IBM Plex Mono', 'monospace'],
+      sans: "Roboto, sans-serif",
+      mono: ["IBM Plex Mono", "monospace"],
     },
   },
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+    base: false,
+    darkTheme: false,
+    styled: true,
+    utils: true,
+    logs: false,
+  },
+};
