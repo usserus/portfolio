03db933681a58a85ce9c13bc73195ee22e2b566f
emil.dueringer <s2210456008@fhooe.at>
2025-09-27 19:55:41 +0200

    feat: add Intro-section.vue

---
+++ storyblok/about-me/Intro-section.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container mx-auto px-4 py-6 font-mono font-normal">
+        <hr class="h-px bg-gray-200 border-0 dark:bg-gray-700">
+
+        <div v-editable="blok" class="grid grid-cols-3 gap-6 mt-10">
+            <div>
+                <h2 class="text-5xl font-light">
+                    {{ blok.heading }}
+                </h2>
+            </div>
+            <div class="col-span-2">
+                <p class="text-lg">
+                    {{ blok.description }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({ blok: Object })
+</script>
