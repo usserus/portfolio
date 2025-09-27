800fd6bae4e6b670fafb03aa84ec003ce9f4eb41
emil.dueringer <s2210456008@fhooe.at>
2025-09-27 19:13:34 +0200

    chore: format code

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,26 +1,21 @@
 export default defineNuxtConfig({
-    css: ['@/assets/css/roboto.css'],
-    modules: [
-        [
-            '@storyblok/nuxt',
-            {
-                accessToken: 'jgZ2j5Qjvkc8mNFfXVSPBQtt',
-                apiOptions: {
-                    region: '' // Set 'US" if your space is created in US region (EU default)
-                }
-            },
-        ],
-        '@nuxtjs/tailwindcss',
-        'nuxt-icon',
+  css: ["@/assets/css/roboto.css"],
+  modules: [
+    [
+      "@storyblok/nuxt",
+      {
+        accessToken: "jgZ2j5Qjvkc8mNFfXVSPBQtt",
+        apiOptions: {
+          region: "", // Set 'US" if your space is created in US region (EU default)
+        },
+      },
     ],
-    vite: {
-        server: {
-            hmr: false
-        }
-    },
-    head: {
-        link: [
-            {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon.ico'},
-        ],
-    }
-})
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+  ],
+  head: {
+    link: [
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon.ico" },
+    ],
+  },
+});
