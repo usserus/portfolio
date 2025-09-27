bcd5b962472ad01c625260950a72464aa91e785f
emil.dueringer <s2210456008@fhooe.at>
2025-09-27 19:04:16 +0200

    feat: add Timeline.vue

---
+++ storyblok/about-me/Timeline.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container mx-auto px-4 py-6 lg:py-14 font-mono font-normal mt-10">
+        <h2>{{ blok.heading }}</h2>
+        <h3>{{ blok.subheading }}</h3>
+        <StoryblokComponent v-for="blok in blok.milestones" :key="blok._uid" :blok="blok" />
+    </div>
+</template>
+
+<script setup>
+defineProps({ blok: Object })
+</script>
