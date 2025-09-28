4fcfe1c9bcc600bb347efcbec74dda94a49ef2fb
emil.dueringer <s2210456008@fhooe.at>
2025-09-28 21:26:27 +0200

    feat: add mobile text sizes

--- storyblok/home/Hero.vue
+++ storyblok/home/Hero.vue
@@ -4,7 +4,8 @@
     <div v-editable="blok" class="mt-32 lg:mt-44">
       <h2 class="text-base font-mono font-light uppercase">{{ blok.subheadline }}</h2>
       <hr class="h-px mt-2 mb-3 bg-gray-500 border-0  w-1/4">
-      <h1 class="lg:w-3/4 text-3xl lg:text-8xl font-mono font-normal uppercase tracking-tighter [word-spacing:-0.4em]">
+      <h1
+        class="lg:w-3/4 text-3xl sm:text-5xl md:text-7xl xl:text-8xl font-mono font-normal uppercase tracking-tighter [word-spacing:-0.4em]">
         {{ blok.headline }}
       </h1>
     </div>
