c27d2a244a3e67bc1913cd4ae21fe5a3c6453591
emil.dueringer <s2210456008@fhooe.at>
2025-09-28 19:34:56 +0200

    feat: add swiper.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,14 @@
 export default defineNuxtConfig({
   css: ["@/assets/css/roboto.css"],
-  modules: [
-    [
-      "@storyblok/nuxt",
-      {
-        accessToken: "jgZ2j5Qjvkc8mNFfXVSPBQtt",
-        apiOptions: {
-          region: "", // Set 'US" if your space is created in US region (EU default)
-        },
+  modules: [[
+    "@storyblok/nuxt",
+    {
+      accessToken: "jgZ2j5Qjvkc8mNFfXVSPBQtt",
+      apiOptions: {
+        region: "", // Set 'US" if your space is created in US region (EU default)
       },
-    ],
-    "@nuxtjs/tailwindcss",
-    "nuxt-icon",
-  ],
+    },
+  ], "@nuxtjs/tailwindcss", "nuxt-icon", "nuxt-swiper"],
   head: {
     link: [
       { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon.ico" },
