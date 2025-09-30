f87633bea1be5c68e7b6b19fac40d9ea38261cf9
emil.dueringer <s2210456008@fhooe.at>
2025-09-30 13:33:37 +0200

    feat: add image filters

--- storyblok/about-me/Image-pair-slider.vue
+++ storyblok/about-me/Image-pair-slider.vue
@@ -6,7 +6,7 @@
             <div class="col-span-2 overflow-hidden mb-4 sm:mb-0">
                 <swiper-container ref="leftSwiperOptions" :init="false">
                     <swiper-slide v-for="(slide, idx) in blok?.slider_left" :key="idx">
-                        <img :src="slide.image.filename" class="w-full h-full object-cover">
+                        <img :src="slide.image.filename" class="w-full h-full object-cover grayscale contrast-[1.1]">
                     </swiper-slide>
                 </swiper-container>
             </div>
@@ -17,7 +17,8 @@
                     <swiper-container ref="rightSwiperOptions" :init="false">
                         <swiper-slide v-for="(slide, idx) in blok?.slider_right" :key="idx"
                             class="flex flex-row items-end">
-                            <img :src="slide.image.filename" class="w-full h-full object-cover">
+                            <img :src="slide.image.filename"
+                                class="w-full h-full object-cover grayscale contrast-[1.1]">
                         </swiper-slide>
                     </swiper-container>
                 </div>
