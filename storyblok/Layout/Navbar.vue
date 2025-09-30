10a8c43761c132cb529529cd3e9a42ce4bb47fce
emil.dueringer <s2210456008@fhooe.at>
2025-09-30 13:52:18 +0200

    feat: adjust router back

--- storyblok/Layout/Navbar.vue
+++ storyblok/Layout/Navbar.vue
@@ -2,7 +2,10 @@
   <nav :class="classes">
     <div class="flex justify-between">
       <div>
-        <NuxtLink :to="link" class="text-sm px-8 py-2 hover:underline">{{ linkText }}</NuxtLink>
+        <button v-if="route.path !== '/'" @click="goBack" class="text-sm px-8 py-2 hover:underline">
+          &lt; back
+        </button>
+        <NuxtLink v-else to="#" class="text-sm px-8 py-2 hover:underline">EmilDueringer ©</NuxtLink>
       </div>
       <div v-if="route.path === '/'" class="uppercase">
         <NuxtLink to="#contact" class="text-sm px-8 py-2 hover:underline">Contact</NuxtLink>
@@ -14,12 +17,16 @@
 <script setup lang="ts">
 
 import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
-const route = useRoute()
 
-const link = computed(() => (route.path === '/' ? '#' : '/'))
-const linkText = computed(() => (route.path === '/' ? 'EmilDueringer ©' : '< back'))
+const route = useRoute();
+const router = useRouter()
+
+const goBack = () => {
+  router.back();
+}
+
 const classes = computed(() =>
   route.path === '/'
     ? 'fixed top-0 left-0 right-0 z-10 py-3 px-4 font-mono font-light bg-white'
