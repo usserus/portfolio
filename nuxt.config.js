d8d7bcaf5d074227dde3dbfd9b8b3f55d2b563f7
emil.dueringer <s2210456008@fhooe.at>
2025-09-30 11:47:33 +0200

    feat: add vueuse

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,20 @@
 export default defineNuxtConfig({
   css: ["@/assets/css/roboto.css"],
-  modules: [[
-    "@storyblok/nuxt",
-    {
-      accessToken: "jgZ2j5Qjvkc8mNFfXVSPBQtt",
-      apiOptions: {
-        region: "", // Set 'US" if your space is created in US region (EU default)
+  modules: [
+    [
+      "@storyblok/nuxt",
+      {
+        accessToken: "jgZ2j5Qjvkc8mNFfXVSPBQtt",
+        apiOptions: {
+          region: "", // Set 'US" if your space is created in US region (EU default)
+        },
       },
-    },
-  ], "@nuxtjs/tailwindcss", "nuxt-icon", "nuxt-swiper"],
+    ],
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "nuxt-swiper",
+    "@vueuse/nuxt",
+  ],
   head: {
     link: [
       { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon.ico" },
