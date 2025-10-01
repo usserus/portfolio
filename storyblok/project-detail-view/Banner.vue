8b42f5b97d3b6d5862a5d3e73d671c90a861c583
emil.dueringer <s2210456008@fhooe.at>
2025-10-01 15:39:42 +0200

    feat: adjust to full width banner

--- storyblok/project-detail-view/Banner.vue
+++ storyblok/project-detail-view/Banner.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto w-10/12 my-10 lg:my-32">
     <div v-editable="blok">
-      <img :src="blok.image.filename" :alt="blok.image.alt">
+      <img :src="blok.image.filename" :alt="blok.image.alt" class="w-full">
     </div>
   </div>
 </template>
