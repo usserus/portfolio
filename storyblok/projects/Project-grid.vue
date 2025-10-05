ad35b1396ac619354c6ef9492a22fe76a5f2c5a9
emil.dueringer <s2210456008@fhooe.at>
2025-10-05 14:18:31 +0200

    feat: adjust 2xl grid layout

--- storyblok/projects/Project-grid.vue
+++ storyblok/projects/Project-grid.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto px-4 py-6 lg:py-16 font-mono font-normal">
     <h1 class="text-5xl my-6">{{ blok.heading }}</h1>
     <p class="mb-10">{{ blok.description }}</p>
-    <div v-editable="blok" class="grid grid-cols-1 md:grid-cols-2 gap-4 pb-10">
+    <div v-editable="blok" class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4 pb-10">
       <StoryblokComponent v-for="blok in blok.content" :key="blok._uid" :blok="blok" />
     </div>
   </div>
