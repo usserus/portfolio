acb4157ecc89b007046c07e6d89aacf066889f75
emil.dueringer <s2210456008@fhooe.at>
2025-10-05 14:07:35 +0200

    feat: adjust heading and layout

--- storyblok/home/About-me.vue
+++ storyblok/home/About-me.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container mx-auto px-4 py-6 lg:py-14 font-mono font-normal">
+  <div class="container mx-auto px-4 py-10 lg:py-14 font-mono font-normal">
 
     <div class="grid lg:grid-cols-2 gap-4 lg:mt-8">
       <div class="w-3/4">
-        <h2 v-editable="blok" class="text-5xl mb-6">
+        <h1 v-editable="blok" class="text-5xl mb-6">
           {{ blok.heading }}
-        </h2>
+        </h1>
       </div>
 
       <div class="grid lg:grid-cols-2 gap-4 lg:my-0 my-4">
