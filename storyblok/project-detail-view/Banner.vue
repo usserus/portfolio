1c8e12945a195150128dfed462405e5e371abf19
emil.dueringer <s2210456008@fhooe.at>
2025-10-07 15:15:10 +0200

    feat: adjust spacing banner

--- storyblok/project-detail-view/Banner.vue
+++ storyblok/project-detail-view/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto px-4 w-10/12 my-10 lg:my-32">
+  <div class="container mx-auto px-4 w-4/5 my-10 lg:my-32">
     <div v-editable="blok">
       <img :src="blok.image.filename" :alt="blok.image.alt" class="w-full">
     </div>
