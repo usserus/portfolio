a5d344d0f323a05a68bc407e046d884fddd39d02
emil.dueringer <s2210456008@fhooe.at>
2025-10-12 15:41:50 +0200

    feat: replace contact form with icons

--- storyblok/home/Contact.vue
+++ storyblok/home/Contact.vue
@@ -1,107 +1,65 @@
 <template>
-
-  <div class="my-14">
-
-  <div v-editable="blok" class="text-5xl font-mono font-normal marquee uppercase" id="contact">
-    <h1>{{ blok.heading }}</h1>
-  </div>
-
-  <div class="container mx-auto px-4 mt-20 font-mono font-light">
-
-    <div class="grid lg:grid-cols-2 gap-4 mt-8">
-
-      <div class="text-xl">
-        <div v-html="articleContent"></div>
-      </div>
-
-      <form action="mailto:emil.dueringer@gmail.com" method="post" enctype="text/plain">
-        <div class="grid md:grid-cols-2 md:gap-6">
-          <div class="relative z-0 w-full mb-5 group">
-            <input type="text" name="floating_first_name" id="floating_first_name"
-                   class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                   placeholder=" " required/>
-            <label for="floating_first_name"
-                   class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First
-              name</label>
-          </div>
-          <div class="relative z-0 w-full mb-5 group">
-            <input type="text" name="floating_last_name" id="floating_last_name"
-                   class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                   placeholder=" " required/>
-            <label for="floating_last_name"
-                   class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last
-              name</label>
-          </div>
-        </div>
-
-        <div class="relative z-0 w-full mb-5 group">
-          <input type="email" name="floating_email" id="floating_email"
-                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                 placeholder=" " required/>
-          <label for="floating_email"
-                 class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email
-            address</label>
-        </div>
-
-        <div class="relative z-0 w-full mb-5 group">
-          <label for="message" class="block mb-2 text-sm text-gray-400">Message</label>
-          <textarea id="message" rows="4"
-                    class="block p-2.5 w-full text-xs text-gray-900 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400"
-                    placeholder="...."/>
-        </div>
-
-        <button type="submit"
-                class="relative px-5 py-2 overflow-hidden text-gray-600 bg-gray-100 border border-gray-100 shadow-inner group">
-          <span
-              class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
-          <span
-              class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
-          <span
-              class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-          <span
-              class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-          <span
-              class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-          <span
-              class="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Submit request</span>
-        </button>
-
-      </form>
-
+  <div class="py-14 bg-white text-black">
+    <div v-editable="blok" class="text-5xl font-mono font-normal marquee uppercase" id="contact">
+      <h1>{{ blok.heading }}</h1>
     </div>
-
+    <div class="container mx-auto justify-items-center px-4 mt-20 font-mono font-light">
+      <div class="flex flex-col max-w-3xl gap-8 mt-8">
+        <div class="text-xl text-center">
+          <div v-html="articleContent"></div>
+        </div>
+        <div class="flex justify-center gap-10">
+          <a href="mailto:emil.dueringer@gmail.com" class="tooltip tooltip-neutral" data-tip="emil.dueringer@gmail.com">
+            <Icon name="tdesign:mail" color="black" class="w-6 h-6" />
+          </a>
+          <a href="tel:+4369910652585" class="tooltip tooltip-neutral" data-tip="+4369910456285">
+            <Icon name="tdesign:call" color="black" class="w-6 h-6" />
+          </a>
+          <a href="https://www.linkedin.com/in/emil-düringer-0a9a661b9" target="_blank" rel="noopener noreferrer"
+            class="tooltip tooltip-neutral" data-tip="Emil Düringer">
+            <svg width="24" height="24" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0_37_103)">
+                <path
+                  d="M0 1.79063C0 0.801563 0.821875 0 1.83594 0H23.1641C24.1781 0 25 0.801563 25 1.79063V23.2094C25 24.1984 24.1781 25 23.1641 25H1.83594C0.821875 25 0 24.1984 0 23.2094V1.79063ZM7.72344 20.9281V9.63906H3.97188V20.9281H7.72344ZM5.84844 8.09688C7.15625 8.09688 7.97031 7.23125 7.97031 6.14688C7.94687 5.03906 7.15781 4.19688 5.87344 4.19688C4.58906 4.19688 3.75 5.04063 3.75 6.14688C3.75 7.23125 4.56406 8.09688 5.82344 8.09688H5.84844ZM13.5172 20.9281V14.6234C13.5172 14.2859 13.5422 13.9484 13.6422 13.7078C13.9125 13.0344 14.5297 12.3359 15.5672 12.3359C16.925 12.3359 17.4672 13.3703 17.4672 14.8891V20.9281H21.2187V14.4531C21.2187 10.9844 19.3688 9.37188 16.9 9.37188C14.9094 9.37188 14.0172 10.4656 13.5172 11.2359V11.275H13.4922L13.5172 11.2359V9.63906H9.76719C9.81406 10.6984 9.76719 20.9281 9.76719 20.9281H13.5172Z"
+                  fill="black" />
+              </g>
+              <defs>
+                <clipPath id="clip0_37_103">
+                  <rect width="25" height="25" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
-
-  </div>
-
-
 </template>
 
 <script setup>
 
-const props = defineProps({blok: Object});
+const props = defineProps({ blok: Object });
 const articleContent = computed(() => renderRichText(props.blok.description));
 
 </script>
 
 <style scoped>
-
 .marquee {
   overflow: hidden;
   white-space: nowrap;
 }
 
 .marquee h1 {
-  animation: scroll-left 12s linear infinite;
+  animation: scroll-left 25s linear infinite;
 }
 
 @keyframes scroll-left {
   0% {
     transform: translateX(0%);
   }
+
   100% {
     transform: translateX(-100%);
   }
 }
-
 </style>
