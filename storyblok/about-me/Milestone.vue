95bd305624ed796f740d12f0b5058ef02c6db7b2
emil.dueringer <s2210456008@fhooe.at>
2025-10-12 14:04:36 +0200

    feat: add organization; adapt description to renderrichtext

--- storyblok/about-me/Milestone.vue
+++ storyblok/about-me/Milestone.vue
@@ -2,19 +2,19 @@
     <div class="font-mono font-light">
         <div v-editable="blok">
             <div class="text-sm uppercase mt-1 mb-2">
-                {{ blok.year }}
+                {{ blok?.year }}
             </div>
             <div class="text-2xl uppercase mt-1">
-                {{ blok.title }}
+                {{ blok?.title }}
             </div>
-
+            <div class="text-sm uppercas">
+                {{ blok?.organization }}
+            </div>
             <details>
                 <summary class="cursor-pointer mt-2 text-sm">
                     read more..
                 </summary>
-                <div class="py-2 text-sm">
-                    <p>{{ blok.description }}</p>
-                </div>
+                <div v-html="description" class="py-2 text-sm"></div>
             </details>
         </div>
         <hr class="h-px my-3 bg-gray-400 border-0">
@@ -22,11 +22,9 @@
 </template>
 
 <script setup>
-
-defineProps({
+const props = defineProps({
     blok: Object
 });
+const description = computed(() => renderRichText(props.blok.description));
 
 </script>
-
-<style scoped></style>
