65f215909257f8fca377fc1eeb4b38c63b76d53e
emil.dueringer <s2210456008@fhooe.at>
2025-10-12 14:03:40 +0200

    feat: format code

--- storyblok/project-detail-view/Description.vue
+++ storyblok/project-detail-view/Description.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="container mx-auto px-4 mt-14 mb-14 grid lg:grid-cols-3 gap-2">
-
     <div class="col-span-1"></div>
     <div class="col-span-2">
       <StoryblokComponent v-for="blok in blok.features" :key="blok._uid" :blok="blok" />
     </div>
-
   </div>
 </template>
 
